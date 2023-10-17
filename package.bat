72cedf0302cb28f5b805a87d57b31c480ea27605
milosz08 <gilgamilosz451@gmail.com>
2023-10-17 22:11:00 +0200

    add bat script for maven build project

---
+++ package.bat
@@ -0,0 +1,14 @@
+@echo off
+rem
+rem Copyright (c) 2023 by MULTIPLE AUTHORS
+rem Part of the CS study course project.
+rem
+
+setlocal enabledelayedexpansion
+
+call mvnw clean package install
+
+call mvnw.cmd -pl "client" clean assembly:assembly
+call mvnw.cmd -pl "host" clean assembly:assembly
+
+endlocal
