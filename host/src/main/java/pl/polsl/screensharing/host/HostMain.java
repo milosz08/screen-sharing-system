c39703b2ef603512f2fc47bb4007ac2f73ef2a96
milosz08 <gilgamilosz451@gmail.com>
2023-11-14 20:16:51 +0100

    add logger for invoking swing thread in host app

--- host/src/main/java/pl/polsl/screensharing/host/HostMain.java
+++ host/src/main/java/pl/polsl/screensharing/host/HostMain.java
@@ -4,6 +4,7 @@
  */
 package pl.polsl.screensharing.host;
 
+import lombok.extern.slf4j.Slf4j;
 import pl.polsl.screensharing.host.state.HostState;
 import pl.polsl.screensharing.host.view.HostWindow;
 import pl.polsl.screensharing.lib.gui.AbstractRootFrame;
@@ -11,13 +12,16 @@ import pl.polsl.screensharing.lib.gui.GuiConfig;
 
 import javax.swing.*;
 
+@Slf4j
 public class HostMain {
     public static void main(String[] args) {
         GuiConfig.setDefaultLayout();
         final HostState state = new HostState();
         SwingUtilities.invokeLater(() -> {
+            log.info("Starting GUI thread.");
             final AbstractRootFrame window = new HostWindow(state);
             window.guiInitAndShow();
+            log.info("Initialized application GUI.");
         });
     }
 }
