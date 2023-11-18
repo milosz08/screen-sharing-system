7ad336244297b1dcf59584613381f6c30f7829d7
milosz08 <gilgamilosz451@gmail.com>
2023-11-18 03:00:59 +0100

    applied show/hide screen sharing button

--- host/src/main/java/pl/polsl/screensharing/host/view/fragment/TopToolbar.java
+++ host/src/main/java/pl/polsl/screensharing/host/view/fragment/TopToolbar.java
@@ -28,27 +28,36 @@ public class TopToolbar extends JToolBar {
     private final JAppIconButton startVideoStreamingButton;
     private final JAppIconButton stopVideoStreamingButton;
 
+    private final JAppIconButton showScreenToParticipantsButton;
+    private final JAppIconButton hideScreenFromParticipantsButton;
+
     private final TopToolbarController controller;
 
     public TopToolbar(HostWindow hostWindow) {
-        this.controller = new TopToolbarController(hostWindow);
-        this.hostState = hostWindow.getHostState();
+        controller = new TopToolbarController(hostWindow);
+        hostState = hostWindow.getHostState();
 
-        this.sessionParamsButton = new JAppIconButton("Session settings", HostIcon.SERVER_SETTINGS, true);
-        this.createSessionButton = new JAppIconButton("Create session", HostIcon.ADD_LINK, true);
-        this.removeSessionButton = new JAppIconButton("Remove session", HostIcon.REMOVE_LINK, true, false);
+        sessionParamsButton = new JAppIconButton("Session settings", HostIcon.SERVER_SETTINGS, true);
+        createSessionButton = new JAppIconButton("Create session", HostIcon.ADD_LINK, true);
+        removeSessionButton = new JAppIconButton("Remove session", HostIcon.REMOVE_LINK, true, false);
 
-        this.startVideoStreamingButton = new JAppIconButton("Start streaming", HostIcon.DEBUG_INTERACTIVE_WINDOW, true, false);
-        this.stopVideoStreamingButton = new JAppIconButton("Stop streaming", HostIcon.APPLICATION_ERROR, true, false);
+        startVideoStreamingButton = new JAppIconButton("Start streaming", HostIcon.DEBUG_INTERACTIVE_WINDOW, true, false);
+        stopVideoStreamingButton = new JAppIconButton("Stop streaming", HostIcon.APPLICATION_ERROR, true, false);
+
+        showScreenToParticipantsButton = new JAppIconButton("Show screen", HostIcon.OPEN_QUERY, true, false);
+        hideScreenFromParticipantsButton = new JAppIconButton("Hide screen", HostIcon.STOP_QUERY, true);
 
         initObservables();
 
-        this.sessionParamsButton.addActionListener(e -> controller.openSessionParamsWindow());
-        this.createSessionButton.addActionListener(e -> controller.createSession());
-        this.removeSessionButton.addActionListener(e -> controller.removeSession());
+        sessionParamsButton.addActionListener(e -> controller.openSessionParamsWindow());
+        createSessionButton.addActionListener(e -> controller.createSession());
+        removeSessionButton.addActionListener(e -> controller.removeSession());
 
-        this.startVideoStreamingButton.addActionListener(e -> controller.startVideoStreaming());
-        this.stopVideoStreamingButton.addActionListener(e -> controller.stopVideoStreaming());
+        startVideoStreamingButton.addActionListener(e -> controller.startVideoStreaming());
+        stopVideoStreamingButton.addActionListener(e -> controller.stopVideoStreaming());
+
+        showScreenToParticipantsButton.addActionListener(e -> controller.toggleScreenShowingForParticipants(true));
+        hideScreenFromParticipantsButton.addActionListener(e -> controller.toggleScreenShowingForParticipants(false));
 
         addButtonWithSeparation(sessionParamsButton);
         addSeparator();
@@ -57,6 +66,9 @@ public class TopToolbar extends JToolBar {
         addSeparator();
         addButtonWithSeparation(startVideoStreamingButton);
         addButtonWithSeparation(stopVideoStreamingButton);
+        addSeparator();
+        addButtonWithSeparation(showScreenToParticipantsButton);
+        addButtonWithSeparation(hideScreenFromParticipantsButton);
 
         setFloatable(false);
     }
@@ -84,6 +96,10 @@ public class TopToolbar extends JToolBar {
             final boolean isStreaming = state.getStreamingState().equals(StreamingState.STREAMING);
             startVideoStreamingButton.setEnabled(!isStreaming && isCreated);
             stopVideoStreamingButton.setEnabled(isStreaming && isCreated);
+        });
+        hostState.wrapAsDisposable(hostState.isScreenIsShowForParticipants$(), isShowing -> {
+            showScreenToParticipantsButton.setEnabled(!isShowing);
+            hideScreenFromParticipantsButton.setEnabled(isShowing);
         });
     }
 }
