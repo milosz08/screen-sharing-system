e8fbd4133638adbc44fd16c6069c02265f3127d8
milosz08 <gilgamilosz451@gmail.com>
2023-11-20 16:26:03 +0100

    change Runnable interface to Thread class

--- lib/src/main/java/pl/polsl/screensharing/lib/thread/AbstractPerTickRunner.java
+++ lib/src/main/java/pl/polsl/screensharing/lib/thread/AbstractPerTickRunner.java
@@ -4,19 +4,9 @@
  */
 package pl.polsl.screensharing.lib.thread;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-public abstract class AbstractPerTickRunner implements Runnable {
+public abstract class AbstractPerTickRunner extends Thread {
     private static final int BILION = 1_000_000_000;
     private static final double MAX_FPS = 60.0;
-
-    private final Thread thread;
-    private final AtomicInteger maxFps;
-
-    public AbstractPerTickRunner(int maxFps) {
-        thread = new Thread(this);
-        this.maxFps = new AtomicInteger(maxFps);
-    }
 
     @Override
     public void run() {
@@ -26,7 +16,7 @@ public abstract class AbstractPerTickRunner implements Runnable {
         long timer = 0;
         int drawCount = 0;
 
-        while (thread != null) {
+        while (true) {
             final double drawInterval = BILION / MAX_FPS;
             currentTime = System.nanoTime();
             delta += (currentTime - lastTime) / drawInterval;
@@ -43,16 +33,6 @@ public abstract class AbstractPerTickRunner implements Runnable {
                 timer = 0;
             }
         }
-    }
-
-    public void startThread() {
-        if (!thread.isAlive()) {
-            thread.start();
-        }
-    }
-
-    public void updateMaxFps(int maxFps) {
-        this.maxFps.set(maxFps);
     }
 
     public abstract void onTickUpdate();
