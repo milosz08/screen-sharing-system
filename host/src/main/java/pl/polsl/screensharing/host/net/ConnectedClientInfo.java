c421e08325c4c0f1094b1759b13d93d7b7b9e0e7
milosz08 <gilgamilosz451@gmail.com>
2023-11-24 04:38:05 +0100

    add connected client info class

---
+++ host/src/main/java/pl/polsl/screensharing/host/net/ConnectedClientInfo.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 by MULTIPLE AUTHORS
+ * Part of the CS study course project.
+ */
+package pl.polsl.screensharing.host.net;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+public class ConnectedClientInfo {
+    private final ClientThread clientThread;
+}
