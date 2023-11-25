bd520e106ac370ca4131dfd620f2abda8f303440
milosz08 <gilgamilosz451@gmail.com>
2023-11-25 06:27:05 +0100

    create basic look and feel for connected users dialog window

--- host/src/main/java/pl/polsl/screensharing/host/view/HostIcon.java
+++ host/src/main/java/pl/polsl/screensharing/host/view/HostIcon.java
@@ -15,11 +15,13 @@ public enum HostIcon implements AppIcon {
     APPLICATION_ERROR("ApplicationError"),
     CLOAK_OR_HIDE("CloakOrHide"),
     CURSOR("Cursor"),
+    IN_USER_BY_OTHER_USER("InUseByOtherUser"),
     DEBUG_INTERACTIVE_WINDOW("DebugInteractiveWindow"),
     LOOKUP_GROUP_MEMBERS("LookupGroupMembers"),
     OPEN_QUERY("OpenQuery"),
     REMOVE_LINK("RemoveLink"),
     STOP_QUERY("StopQuery"),
+    TEAM_PROJECT_COLLECTION_OFFLINE("TeamProjectCollectionOffline"),
     VISIBLE("Visible"),
     ;
 
